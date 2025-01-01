79bb807612b9936ef8bd7e00cc8942ec1fd41ce6
Erik Neves <erikrrn04@gmail.com>
2025-01-01 19:23:48 -0300

    test generate statistics

--- tests/test_lzw_decompressor.py
+++ tests/test_lzw_decompressor.py
@@ -1,7 +1,9 @@
+import os
 from FileManager.FileManager import FileManager
 import pytest # type: ignore
 from LZW.Compressor import LZWCompressor
 from LZW.Decompressor import LZWDecompressor
+from tests.helpers.file_manager_helpers import remove_temp_file
 
 # Variáveis de controle
 SIGMA_SIZE = 256
@@ -53,3 +55,16 @@ def test_compressing_and_decompressing_only_dots():
     decompressedContent = decompressor.Decompress(code_control_bits, compressed)
 
     assert content == decompressedContent
+
+def test_decompressor_with_enabled_statatistics():
+    compressor = LZWCompressor(SIGMA_SIZE, CODE_CONTROL_BITS, DEFAULT_CODE_BITS, DEFAULT_CODE_BITS)
+    content = "Em Algoritmos II \n , falhar nos testes não é o fim"
+    result = compressor.Compress(content)
+    code_control_bits, compressed = LZWCompressor.ExtractCodeLenghtAndContent(result, CODE_CONTROL_BITS)
+    
+    decompressor = LZWDecompressor(SIGMA_SIZE, enableStatistics=True)
+    decompressor.Decompress(code_control_bits, compressed)
+
+    file_name = 'decompressed-statistics.csv'
+    assert os.path.exists(file_name)
+    remove_temp_file(file_name)
